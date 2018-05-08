e4200bc9f34a48c4e5b9599215662648a908e265
JoelSpinelli <joel.spinelli@hotmail.com>
2018-05-08 01:30:18 -0300

    commit inicial

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,17 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+    $scope.fotos = [];
+
+    $http.get('/v1/fotos')
+    .success( fotos => $scope.fotos = fotos ).error(erro => console.log(erro));
+
+
+    /*
+    $http.get('/v1/fotos')
+    .success(retorno => {
+        console.log(retorno);
+        $scope.fotos = retorno; // não precisa fazer retorno.data
+    })
+    .error(erro => console.log(erro) );
+    */
+});
