481ef230a28b877109586f193ea15cc200438217
JoelSpinelli <joel.spinelli@hotmail.com>
2018-05-09 01:35:26 -0300

    diminuindo a complexidade do HTML e aplicando filtro na tela

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,9 +1,9 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
     $scope.fotos = [];
+    $scope.filtro = '';
 
-    $http.get('/v1/fotos')
-    .success( fotos => $scope.fotos = fotos ).error(erro => console.log(erro));
+    $http.get('/v1/fotos').success( fotos => $scope.fotos = fotos ).error(erro => console.log(erro));
 
 
     /*
